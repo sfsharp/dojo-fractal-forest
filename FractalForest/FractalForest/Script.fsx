bc4ad34354a1a46d282bf20513bfd03447a99a3c
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2013-09-08 15:08:39 -0700

    Stub: render lines on form
    Script that creates a form and renders lines on it.

---
+++ FractalForest/FractalForest/Script.fsx
@@ -0,0 +1,35 @@
+﻿open System
+open System.Drawing
+open System.Windows.Forms
+
+// Create a form to display the graphics         
+let form = new Form(Width = 500, Height = 500)
+let box = new PictureBox(BackColor = Color.White, Dock = DockStyle.Fill)
+let image = new Bitmap(500, 500)
+let graphics = Graphics.FromImage(image)
+let brush = new SolidBrush(Color.FromArgb(0, 0, 0))
+
+box.Image <- image
+form.Controls.Add(box) 
+
+// Utility function: draw a line of given width, 
+// starting from x, y
+// going at a certain angle, for a certain length,
+let drawLine (target : Graphics) (brush : Brush) x y angle length width =
+    let x_end = x + length * cos angle
+    let y_end = y + length * sin angle
+    let origin = new PointF(x, y)
+    let destination = new PointF(x_end, y_end)
+    let pen = new Pen(brush, width)
+    target.DrawLine(pen, origin, destination)
+
+let draw x y angle length width = 
+    drawLine graphics brush x y angle length width
+
+let pi = Math.PI |> (single)
+
+// Now... your turn to draw
+draw 100.f 100.f pi 50.0f 2.f
+draw 100.f 100.f (pi*(0.5f)) 100.0f 2.f
+
+form.ShowDialog()
