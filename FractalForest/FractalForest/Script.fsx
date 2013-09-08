4700ea8d955623084fad33a6dd539e790ea0751f
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2013-09-08 15:24:53 -0700

    Improved base tree
    Added trunk + 2 branches, and function to compute end of line position.

--- FractalForest/FractalForest/Script.fsx
+++ FractalForest/FractalForest/Script.fsx
@@ -12,12 +12,15 @@ let brush = new SolidBrush(Color.FromArgb(0, 0, 0))
 box.Image <- image
 form.Controls.Add(box) 
 
+let endpoint x y angle length =
+    x + length * cos angle,
+    y + length * sin angle
+
 // Utility function: draw a line of given width, 
 // starting from x, y
-// going at a certain angle, for a certain length,
+// going at a certain angle, for a certain length.
 let drawLine (target : Graphics) (brush : Brush) x y angle length width =
-    let x_end = x + length * cos angle
-    let y_end = y + length * sin angle
+    let x_end, y_end = endpoint x y angle length
     let origin = new PointF(x, y)
     let destination = new PointF(x_end, y_end)
     let pen = new Pen(brush, width)
@@ -29,7 +32,11 @@ let draw x y angle length width =
 let pi = Math.PI |> (single)
 
 // Now... your turn to draw
-draw 100.f 100.f pi 50.0f 2.f
-draw 100.f 100.f (pi*(0.5f)) 100.0f 2.f
+// The trunk
+draw 250.f 400.f (pi*(1.5f)) 100.0f 4.f
+let x, y = endpoint 250.f 400.f (pi*(1.5f)) 100.0f
+// first and second branches
+draw x y (pi*(1.5f + 0.3f)) 50.f 2.f
+draw x y (pi*(1.5f - 0.3f)) 50.f 2.f
 
 form.ShowDialog()
